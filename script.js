779fc04f60a8e43b1154a18cc0bc7280a6ce9454
acekamaa <kvnkamaa@gmail.com>
2025-12-02 13:22:47 +0300

    Add Hovering effect

--- script.js
+++ script.js
@@ -2,8 +2,18 @@ const container = document.querySelector("#container");
 const gridSize = 16;
 const totalSquares = gridSize * gridSize;
 
+// adding square grids 
 for (let i = 0; i < totalSquares; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
     container.appendChild(square);
 }
+
+// adding Hover effect to leave a pixelated path
+const squares = document.querySelectorAll(".square");
+
+squares.forEach(square => {
+  square.addEventListener("mouseover", () => {
+    square.style.backgroundColor = "green"; // trail color
+  });
+});
